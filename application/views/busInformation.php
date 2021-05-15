410390615909de009dd683b7e68acb709de2e3d2
YoojinKazama <eugenekylecancio@gmail.com>
2021-05-15 17:06:35 +0800

    add functioning create bus information

--- application/views/busInformation.php
+++ application/views/busInformation.php
@@ -48,14 +48,14 @@ The above copyright notice and this permission notice shall be included in all c
                       <form id="busInformationForm">
                           <div class="form-row">
                               <div class="form-group col-sm-12">
-                              <label for="busNumberInput">Bus No.</label>
-                              <input type="text" class="form-control" id="busNumberInput">
+                              <label for="busNumberInput">Bus Number</label>
+                              <input type="text" class="form-control" id="busNumberInput" name="busNumberInput">
                               </div>
                           </div>
                           <div class="form-row">
                               <div class="form-group col-sm-6">
                                 <label for="busTypeInput">Bus Type</label>
-                                <select class="form-control" id="busTypeInput">
+                                <select class="form-control" id="busTypeInput" name="busTypeInput">
                                   <option>1</option>
                                   <option>2</option>
                                   <option>3</option>
@@ -65,7 +65,7 @@ The above copyright notice and this permission notice shall be included in all c
                               </div>
                               <div class="form-group col-sm-6">
                                 <label for="busTemplateInput">Bus Template</label>
-                                <select class="form-control" id="busTemplateInput">
+                                <select class="form-control" id="busTemplateInput" name="busTemplateInput">
                                   <option>1</option>
                                   <option>2</option>
                                   <option>3</option>
@@ -77,83 +77,83 @@ The above copyright notice and this permission notice shall be included in all c
                           <div class="form-row">
                               <div class="form-group col-sm-6">
                               <label for="ownerInput">Owner</label>
-                              <input type="text" class="form-control" id="ownerInput">
+                              <input type="text" class="form-control" id="ownerInput" name="ownerInput">
                               </div>
                               <div class="form-group col-sm-6">
                               <label for="contactInput">Contact Number</label>
-                              <input type="text" class="form-control" id="contactInput">
+                              <input type="text" class="form-control" id="contactInput" name="contactInput">
                               </div>
                           </div>
                           <div class="form-row">
                               <div class="form-group col-sm-3">
                               <label for="plateInput">Plate Number</label>
-                              <input type="text" class="form-control" id="plateInput">
+                              <input type="text" class="form-control" id="plateInput" name="plateInput">
                               </div>
                               <div class="form-group col-sm-3">
                               <label for="engineInput">Engine Number</label>
-                              <input type="text" class="form-control" id="engineInput">
+                              <input type="text" class="form-control" id="engineInput" name="engineInput">
                               </div>
                               <div class="form-group col-sm-3">
                               <label for="chasisInput">Chasis Number</label>
-                              <input type="text" class="form-control" id="chasisInput">
+                              <input type="text" class="form-control" id="chasisInput" name="chasisInput">
                               </div>
                               <div class="form-group col-sm-3">
                               <label for="fileNumberInput">MV File Number</label>
-                              <input type="text" class="form-control" id="fileNumberInput">
+                              <input type="text" class="form-control" id="fileNumberInput" name="fileNumberInput">
                               </div>
                           </div>
                           <div class="form-row">
                               <div class="form-group col-sm-3">
                               <label for="denominationInput">Denomination</label>
-                              <input type="text" class="form-control" id="denominationInput">
+                              <input type="text" class="form-control" id="denominationInput" name="denominationInput">
                               </div>
                               <div class="form-group col-sm-3">
                               <label for="pistonInput">Piston Placement</label>
-                              <input type="text" class="form-control" id="pistonInput">
+                              <input type="text" class="form-control" id="pistonInput" name="pistonInput">
                               </div>
                               <div class="form-group col-sm-3">
                               <label for="cylindersInput">Number of Cylinders</label>
-                              <input type="text" class="form-control" id="cylindersInput">
+                              <input type="text" class="form-control" id="cylindersInput" name="cylindersInput">
                               </div>
                               <div class="form-group col-sm-3">
                               <label for="fuelInput">Fuel</label>
-                              <input type="text" class="form-control" id="fuelInput">
+                              <input type="text" class="form-control" id="fuelInput" name="fuelInput">
                               </div>
                           </div>
                           <div class="form-row">
                               <div class="form-group col-sm-3">
                               <label for="makeInput">Make</label>
-                              <input type="text" class="form-control" id="makeInput">
+                              <input type="text" class="form-control" id="makeInput" name="makeInput">
                               </div>
                               <div class="form-group col-sm-3">
                               <label for="seriesInput">Series</label>
-                              <input type="text" class="form-control" id="seriesInput">
+                              <input type="text" class="form-control" id="seriesInput" name="seriesInput">
                               </div>
                               <div class="form-group col-sm-3">
                               <label for="cylindersInput">Body Type</label>
-                              <input type="text" class="form-control" id="bodyInput">
+                              <input type="text" class="form-control" id="bodyInput" name="bodyInput">
                               </div>
                               <div class="form-group col-sm-3">
                               <label for="bodyNumberInput">Body Number</label>
-                              <input type="text" class="form-control" id="bodyNumberInput">
+                              <input type="text" class="form-control" id="bodyNumberInput" name="bodyNumberInput">
                               </div>
                           </div>
                           <div class="form-row">
                               <div class="form-group col-sm-3">
                               <label for="yearInput">Year Model</label>
-                              <input type="text" class="form-control" id="yearInput">
+                              <input type="text" class="form-control" id="yearInput" name="yearInput">
                               </div>
                               <div class="form-group col-sm-3">
                               <label for="grossWeightInput">Gross Weight (kg)</label>
-                              <input type="text" class="form-control" id="grossWeightInput">
+                              <input type="text" class="form-control" id="grossWeightInput" name="grossWeightInput">
                               </div>
                               <div class="form-group col-sm-3">
                               <label for="netWeightInput">Net Weight (kg)</label>
-                              <input type="text" class="form-control" id="netWeightInput">
+                              <input type="text" class="form-control" id="netWeightInput" name="netWeightInput">
                               </div>
                               <div class="form-group col-sm-3">
                               <label for="shipWeightInput">Shipping Weight (kg)</label>
-                              <input type="text" class="form-control" id="shipWeightInput">
+                              <input type="text" class="form-control" id="shipWeightInput" name="shipWeightInput">
                               </div>
                           </div>
                           <div class="form-row">
@@ -161,19 +161,19 @@ The above copyright notice and this permission notice shall be included in all c
                           </div>
                           <div class="form-row">
                             <div class="form-check form-check-inline">
-                              <input  type="checkbox" id="inlineCheckbox1" value="True">
-                              <label class="form-check-label" for="inlineCheckbox1" style="padding-left: 5px;">with Television</label>
+                              <input  type="checkbox" id="withTelevision" name="withTelevision" value="True">
+                              <label class="form-check-label" for="withTelevision" style="padding-left: 5px;">with Television</label>
                             </div>
                             <div class="form-check form-check-inline">
-                              <input  type="checkbox" id="inlineCheckbox2" value="True">
-                              <label class="form-check-label" for="inlineCheckbox2" style="padding-left: 5px;">with Aircon</label>
+                              <input  type="checkbox" id="withAircon" name="withAircon" value="True">
+                              <label class="form-check-label" for="withAircon" style="padding-left: 5px;">with Aircon</label>
                             </div>
                             <div class="form-check form-check-inline">
-                              <input  type="checkbox" id="inlineCheckbox3" value="True">
-                              <label class="form-check-label" for="inlineCheckbox3" style="padding-left: 5px;">with WiFi</label>
+                              <input  type="checkbox" id="withWifi" name="withWifi" value="True">
+                              <label class="form-check-label" for="withWifi" style="padding-left: 5px;">with WiFi</label>
                             </div>
                           </div>
-                          <button type="submit" class="btn btn-primary">Submit</button>
+                          <input type="submit" class="btn btn-primary">
                       </form>
                   </div>
                   </div>
@@ -202,6 +202,26 @@ The above copyright notice and this permission notice shall be included in all c
   <?php $this->load->view('includes/core_js_files.php')?>
   
 </body>
+
+<script>
+$('#busInformationForm').on('submit', function(e){
+
+    e.preventDefault();
+
+    var form = $('#busInformationForm');
+
+    $.ajax({
+        url:'<?php echo base_url()?>busInformation/addBusInformation',
+        type: "POST",
+        data: form.serialize(),
+        dataType: "JSON",
+
+        success: function(data){
+          
+        }
+    })
+});
+</script>
 
 <!-- FIXED SCRIPTS -->
 <?php $this->load->view('includes/fixed_scripts.php')?>
