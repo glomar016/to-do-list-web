b211f8b8dc92618f3007e9ab0ae70e33bb46712d
arcadepreman <jollyman345@gmail.com>
2021-05-17 04:11:02 +0800

    added create and datatable in views/landmark

--- application/views/Landmark.php
+++ application/views/Landmark.php
@@ -153,6 +153,7 @@ The above copyright notice and this permission notice shall be included in all c
   
 </body>
 <script>
+
  //DATA TABLES
    function loadtable(){
        landmarkDataTable = $('#landmarkTable').DataTable( {
