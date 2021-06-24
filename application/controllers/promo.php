63e29de767a24bfb95f18c694a88ea3752e2b33d
jeff021 <jeffersonborjal019@gmail.com>
2021-06-24 17:55:41 +0800

    added promo controller

---
+++ application/controllers/promo.php
@@ -0,0 +1,199 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Promo extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('promo');
+	}
+
+    public function add_promo()
+    {
+        $codeInput = $this->input->post('codeInput');
+        $busTypeInput = $this->input->post('busTypeInput');
+        $fixedDiscountInput = $this->input->post('fixedDiscountInput');
+        $percentageDiscountInput = $this->input->post('percentageDiscountInput');
+        $minimumAmountInput = $this->input->post('minimumAmountInput');
+        $minimumSeatReservationInput = $this->input->post('minimumSeatReservationInput');
+        $bookingDateToInput = $this->input->post('bookingDateToInput');
+        $bookingDateFromInput = $this->input->post('bookingDateFromInput');
+        $travelDateToInput = $this->input->post('travelDateToInput');
+        $travelDateFromInput = $this->input->post('travelDateFromInput');
+        $effectivityDateInput = $this->input->post('effectivityDateInput');
+        $deactivationDateInput = $this->input->post('deactivationDateInput');
+
+        $data = array("code" => $codeInput
+						// , "busTypeId" => $busTypeInput
+						, "fixedDiscount" => $fixedDiscountInput
+                        , "percentageDiscount" => $percentageDiscountInput
+                        , "minimumAmount" => $minimumAmountInput
+                        , "minimumSeatReservation" => $minimumSeatReservationInput
+                        , "bookingDateTo" => $bookingDateToInput
+                        , "bookingDateFrom" => $bookingDateFromInput
+                        , "tavelDateTo" => $travelDateToInput
+                        , "travelDateFrom" => $travelDateFromInput
+                        , "effectivityDate" => $effectivityDateInput
+						, "deactivationDate" => $deactivationDateInput);
+
+		$postdata = json_encode($data);
+
+		$curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'http://localhost:3600/api/v1/promo/',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $postdata,
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+    }
+
+	public function show_promo(){
+		$curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'http://localhost:3600/api/v1/promo/',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+
+	}
+
+	public function get_one_promo(){
+
+		$id = $this->input->post('id');
+			
+		$curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'http://localhost:3600/api/v1/promo/'.$id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+	}
+
+	public function edit_promo(){
+		$codeEdit = $this->input->post('codeEdit');
+        // $busTypeEdit = $this->input->post('busTypeEdit');
+        $fixedDiscountEdit = $this->input->post('fixedDiscountEdit');
+        $percentageDiscountEdit = $this->input->post('percentageDiscountEdit');
+        $minimumAmountEdit = $this->input->post('minimumAmountEdit');
+        $minimumSeatReservationEdit = $this->input->post('minimumSeatReservationEdit');
+        $bookingDateToEdit = $this->input->post('bookingDateToEdit');
+        $bookingDateFromEdit = $this->input->post('bookingDateFromEdit');
+        $travelDateToEdit = $this->input->post('travelDateToEdit');
+        $travelDateFromEdit = $this->input->post('travelDateFromEdit');
+        $effectivityDateEdit = $this->input->post('effectivityDateEdit');
+        $deactivationDateEdit = $this->input->post('deactivationDateEdit');
+        $id = $this->input->post('promoIdEdit');
+
+        $data = array("code" => $codeEdit
+						, "busTypeId" => $busTypeEdit
+						, "fixedDiscount" => $fixedDiscountEdit
+                        , "percentageDiscount" => $percentageDiscountEdit
+                        , "minimumAmount" => $minimumAmountEdit
+                        , "minimumSeatReservation" => $minimumSeatReservationEdit
+                        , "bookingDateTo" => $bookingDateToEdit
+                        , "bookingDateFrom" => $bookingDateFromEdit
+                        , "tavelDateTo" => $travelDateToEdit
+                        , "travelDateFrom" => $travelDateFromEdit
+                        , "effectivityDate" => $effectivityDateEdit
+						, "deactivationDate" => $deactivationDateEdit);
+
+		$postdata = json_encode($data);
+
+		$curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'http://localhost:3600/api/v1/promo/'.$id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'PUT',
+        CURLOPT_POSTFIELDS => $postdata,
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/json'
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+
+	}
+
+	public function delete_promo(){
+        $id = $this->input->post('deletePromoId');
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'http://localhost:3600/api/v1/promo/'.$id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'DELETE',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+    }
+
+}
---
+++ application/views/promo.php
@@ -0,0 +1,423 @@
+<!--
+=========================================================
+Material Dashboard - v2.1.2
+=========================================================
+
+Product Page: https://www.creative-tim.com/product/material-dashboard
+Copyright 2020 Creative Tim (https://www.creative-tim.com)
+Coded by Creative Tim
+
+=========================================================
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. -->
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- HEAD TAG -->
+<?php $this->load->view('includes/head.php'); ?>
+
+<style>
+#editUserForm .form-row,
+#addUserForm .form-row{
+  margin-top: 15px;
+}
+</style>
+
+<body class="">
+
+  <!-- WRAPPER -->
+  <div class="wrapper ">
+
+    <!-- SIDEBAR -->
+    <?php $this->load->view('includes/sidebar.php'); ?>
+    
+    <!-- MAIN CONTENT -->
+    <div class="main-panel">
+
+      <!-- NAVBAR -->
+      <?php $this->load->view('includes/navbar.php'); ?>
+
+      <!-- OPENING TAG OF CONTENT -->
+      <div class="content" style="padding-top: 0px;">
+        <div class="container-fluid">
+          <div class="row">
+            <!-- END OF OPENING TAG OF CONTENT -->
+            <div class="card">
+              <h5 class="card-header">
+                  <a class="collapsed d-block" data-toggle="collapse" href="#collapse-collapsed" aria-expanded="true" aria-controls="collapse-collapsed" id="heading-collapsed">
+                      <i class="fa fa-chevron-down pull-right"></i>
+                      Add Promo
+                  </a>
+              </h5>
+              <div id="collapse-collapsed" class="collapse" aria-labelledby="heading-collapsed">
+                  <div class="card-body">
+                  <div class="card-body">
+                  <form id="addUserForm" name="addUserForm">
+                      <div class="form-row">
+                        <div class="form-group col-sm-6">
+                            <label for="exampleInputEmail1">First Name</label>
+                            <input type="text" class="form-control" name="firstName" id="firstName" aria-describedby="emailHelp">
+                        </div>
+                        <div class="form-group col-sm-6">
+                            <label for="exampleInputEmail1">Last Name</label>
+                            <input type="text" class="form-control" name="lastName"  id="lastName" aria-describedby="emailHelp">
+                        </div>
+                      </div>
+                      <div class="form-row">
+                        <div class="form-group col-sm-12">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" name="email"  id="email" aria-describedby="emailHelp">
+                        </div>
+                      </div>
+                      <div class="form-row">
+                            <div class="form-group col-sm-12">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" name="password"  class="form-control" id="password" >
+                            </div>
+                        </div>
+                        <input type="submit" class="btn btn-primary">
+                    </form>
+                  </div>
+                  </div>
+              </div>
+          </div>
+            <div class="card">
+              <div class="card-body">
+                <table id="userTable" class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Promo</th>
+                            <th>Effectivity Date</th>
+                            <th>Deactivation Date</th>
+                            <th>Date Created</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+              </div>
+            </div>
+
+            <!-- CLOSING TAG OF CONTENT -->
+          </div>
+        </div>
+      </div>
+
+      <!-- END OF CLOSING TAG OF CONTENT -->
+
+      <!-- VIEW USER MODAL -->
+      <div class="modal fade" id="viewModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="viewModal">View User</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+            <div class="card-body">
+            <form id="viewUserForm">
+                <div class="modal-body">
+                    <div class="form-row">
+                      <div class="form-group col-sm-6">
+                        <!-- <input hidden type="text" class="form-control" name="viewUserId" id="viewUserId" aria-describedby="emailHelp"> -->
+                        <label for="viewFirstName">First Name</label>
+                        <span class="form-control" name="viewFirstName" id="viewFirstName" aria-describedby="emailHelp"></span>
+                      </div>
+                      <div class="form-group col-sm-6">
+                        <label for="viewLastName">Last Name</label>
+                        <span class="form-control" name="viewLastName" id="viewLastName" aria-describedby="emailHelp"></span>
+                      </div>
+                    </div>
+                    <div class="form-row">
+                      <div class="form-group col-sm-12">
+                        <label for="viewEmail">Email</label>
+                        <span class="form-control" name="viewEmail" id="viewEmail" aria-describedby="emailHelp"></span>
+                      </div>
+                    </div>
+                </div>
+                </form>
+            </div>
+          </div>
+        </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- EDIT USER MODAL -->
+      <div class="modal fade" id="editUserInfoModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="editUserInfoModal">Edit User</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+            <div class="card-body">
+            <form id="editUserForm">
+                <div class="modal-body">
+                <div class="form-row">
+                      <div class="form-group col-sm-6">
+                        <input hidden type="text" class="form-control" name="editUserId" id="editUserId" aria-describedby="emailHelp">
+                        <label for="editFirstName">First Name</label>
+                        <input type="text" class="form-control" name="editFirstName" id="editFirstName" aria-describedby="emailHelp">
+                      </div>
+                      <div class="form-group col-sm-6">
+                        <label for="editLastName">Last Name</label>
+                        <input type="text" class="form-control" name="editLastName" id="editLastName" aria-describedby="emailHelp">
+                      </div>
+                    </div>
+                    <div class="form-row">
+                      <div class="form-group col-sm-12">
+                        <label for="editEmail">Email</label>
+                        <input type="text" class="form-control" name="editEmail" id="editEmail" aria-describedby="emailHelp">
+                      </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input type="submit" class="btn btn-primary">
+                </div>
+            </form>
+            </div>
+          </div>
+        </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- DELETE USER MODAL -->
+      <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Are you sure?</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form id="deleteForm">
+              <input hidden type="text" id="deleteUserId" name="deleteUserId">
+              You're deactivating this user
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+              <button type="button" class="btn btn-danger delete-confirm" >Deactivate</button>
+            </div>
+          </div>
+        </div>
+      </div> 
+
+      
+      <!-- FOOTER -->
+      <?php $this->load->view('includes/footer.php')?>
+
+    </div>
+    <!-- END OF MAIN CONTENT -->
+
+  </div>
+  <!-- END OF WRAPPER -->
+
+  <!-- FIXED PLUGINS -->
+  
+  <!-- FIXED PLUGINS -->
+  <?php $this->load->view('includes/core_js_files.php')?>
+  
+</body>
+<script>
+    // DATA TABLES
+    function loadtable(){
+        userDataTable = $('#userTable').DataTable( {
+            "ajax": "<?php echo base_url()?>user/show_user",
+            "columns": [
+                { data: "id"},
+                { data: "firstName", render: function(data, type, row){
+                        return `${row.firstName} ${row.lastName}`
+                    }
+                },
+                { data: "lastName"},
+                { data: "email"},
+                { data: "created_at" },
+                { data: "status", render: function(data, type, row){
+                        if(data == "Active"){
+                            return '<div class="btn-group">'+
+                                    '<button class="btn btn-primary btn-sm btn_view" value="'+row.id+'" title="View" type="button" ><i class="zmdi zmdi-eye"></i>View</button>'+
+                                    '<button class="btn btn-warning btn-sm btn_edit" value="'+row.id+'" title="Edit" type="button" ><i class="zmdi zmdi-edit"></i>Edit</button>'+
+                                    '<button class="btn btn-danger btn-sm btn_delete" value="'+row.id+'" title="Delete" type="button"> <i class="zmdi zmdi-delete"></i>Delete</button></div>';
+                        }   
+                        else{
+                            return '<button>Activate</button>';
+                        }
+                    }
+                    
+                },
+
+            ],
+
+            "aoColumnDefs": [{ "bVisible": false, "aTargets": [0, 4] }],
+            "order": [[4, "desc"]]
+        })
+    }
+
+    
+
+    loadtable();
+    
+    function refresh(){
+        var url = "<?php echo base_url()?>user/show_user";
+
+        userDataTable.ajax.url(url).load();
+    }
+
+    // CREATE USER 
+    $('#addUserForm').on('submit', function(e){
+        e.preventDefault();
+
+        var form = $('#addUserForm'); 
+
+        // ajax opening tag
+        $.ajax({
+            url: '<?php echo base_url()?>user/add_user',
+            type: "POST",
+            data: form.serialize(),
+        
+            success: function(data){
+              document.getElementById("addUserForm").reset();
+              showNotification('create', 'Successfully added a new user!', 'success', 'top', 'right');
+              refresh();
+
+            }
+        // ajax closing tag
+        })
+    });
+
+    // VIEW USER 
+    $(document).on("click", ".btn_view", function(){
+        var id = this.value;
+        // console.log(id);
+
+        $.ajax({
+            url: '<?php echo base_url()?>user/get_one_user',
+            type: "POST",
+            data: { id: id },
+            dataType: "JSON",
+        
+            success: function(data){
+                console.log(data);
+                var userInfo = data.data;
+
+                $('#viewUserId').val(id);
+                $('#viewFirstName').html(userInfo.firstName);
+                $('#viewLastName').html(userInfo.lastName);
+                $('#viewEmail').html(userInfo.email);
+
+                $('#viewModal').modal('show');
+            }
+        // ajax closing tag
+        })
+    });
+
+    // EDIT USER 
+    $(document).on("click", ".btn_edit", function(){
+        var id = this.value;
+        // console.log(id);
+
+        $.ajax({
+            url: '<?php echo base_url()?>user/get_one_user',
+            type: "POST",
+            data: { id: id },
+            dataType: "JSON",
+        
+            success: function(data){
+                console.log(data);
+                var userInfo = data.data;
+
+                $('#editUserId').val(id);
+                $('#editFirstName').val(userInfo.firstName);
+                $('#editLastName').val(userInfo.lastName);
+                $('#editEmail').val(userInfo.email);
+
+                $('#editUserInfoModal').modal('show');
+            }
+        // ajax closing tag
+        })
+    });
+
+    $('#editUserForm').on('submit', function(e){
+        e.preventDefault();
+
+        console.log('working');
+
+        var form = $('#editUserForm'); 
+
+        // ajax opening tag
+        $.ajax({
+            url: '<?php echo base_url()?>user/edit_user',
+            type: "POST",
+            data: form.serialize(),
+            dataType: "JSON",
+        
+            success: function(data){
+              $("#editUserInfoModal").modal('hide');
+                showNotification('update', 'Successfully update user!', 'warning', 'top', 'right');
+                refresh();
+
+            }
+        // ajax closing tag
+        })
+    });
+
+  // DELETE USER
+$(document).on("click", ".btn_delete", function(){
+  var id = this.value;
+  $("#deleteModal").modal('show');
+  $.ajax({
+        url:'<?php echo base_url()?>user/get_one_user',
+        type: "POST",
+        data: { id: id},
+        dataType: "JSON",
+
+        success: function(data){
+          var userInfo = data.data;
+          var deleteUserId = document.getElementById('deleteUserId');
+          deleteUserId.value = userInfo.id
+        }
+    })
+});
+
+$('.delete-confirm').on('click', function(e){
+
+    e.preventDefault();
+
+    var form = $('#deleteForm');
+
+    $.ajax({
+        url:'<?php echo base_url()?>user/delete_user',
+        type: "POST",
+        data: form.serialize(),
+        dataType: "JSON",
+
+        success: function(data){
+          $("#deleteModal").modal('hide');
+          showNotification('delete', 'Deleted a user!', 'danger', 'top', 'right');
+          refresh();
+        }
+    })
+});
+
+</script>
+
+<!-- FIXED SCRIPTS -->
+<?php $this->load->view('includes/fixed_scripts.php')?>
+
+</html>
+
+
